1bcc52f117061bad45192690899c9ccdbbef8578
Nipun Nirman <164348907+nipunnirman@users.noreply.github.com>
2026-01-31 17:51:33 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
+
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  base: "/portfolio-website/", // This matches your repository name
 })
